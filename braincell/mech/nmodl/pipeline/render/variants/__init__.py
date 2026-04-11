494941dc4c70ea1e01ba6c5128d09c6466801850
Chaoming Wang <wcm.brainsim@gmail.com>
2026-04-11 13:04:04 +0800

    remove `mod_validate` and `nmodl`

--- braincell/mech/nmodl/pipeline/render/variants/__init__.py
+++
@@ -1,9 +0,0 @@
-from .braincell_one_ion_hh_ohmic import VARIANT_NAME
-from .braincell_one_ion_hh_ohmic import render_braincell_one_ion_hh_ohmic
-from .braincell_one_ion_hh_ohmic import run
-
-__all__ = [
-    "VARIANT_NAME",
-    "render_braincell_one_ion_hh_ohmic",
-    "run",
-]
